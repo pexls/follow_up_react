50fae178ae55a284f045024ad65ff2e4cdb67b34
mohammed al maslamani <mw.iiiijob@gmail.com>
2023-02-07 15:45:26 +0300

    new

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <header style={headerStyles}>
+      <h1>Header</h1>
+    </header>
+  );
+}
+
+function Body() {
+  return (
+    <main style={bodyStyles}>
+      <h1>Body</h1>
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer style={footerStyles}>
+      <h1>Footer</h1>
+    </footer>
+  );
+}
+
+const headerStyles = {
+  background: 'lightblue',
+  fontSize: '24px',
+  textAlign: 'center',
+  
+};
+
+const bodyStyles = {
+  background: 'lightgray',
+  fontSize: '20px',
+  textAlign: 'center'
+};
+
+const footerStyles = {
+  background: 'darkgray',
+  fontSize: '18px',
+  textAlign: 'center'
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
